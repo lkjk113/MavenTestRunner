f2a55a636dc1520423b51253bbbd704a509578c5
John Yue <yh113@sina.com>
2018-08-03 12:54:41 +0800

    Created

---
+++ TestRunner/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace TestRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.txtFeaturesPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFeaturesFolder = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSortByName = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.lbxSort = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtResults = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProjectPath = new System.Windows.Forms.Button();
+            this.txtProjectPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.treeView1.Location = new System.Drawing.Point(44, 46);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(795, 211);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeCheck += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCheck);
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            // 
+            // txtFeaturesPath
+            // 
+            this.txtFeaturesPath.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtFeaturesPath.Location = new System.Drawing.Point(145, 19);
+            this.txtFeaturesPath.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtFeaturesPath.Name = "txtFeaturesPath";
+            this.txtFeaturesPath.Size = new System.Drawing.Size(639, 21);
+            this.txtFeaturesPath.TabIndex = 1;
+            this.txtFeaturesPath.TextChanged += new System.EventHandler(this.txtFolderPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(40, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Features Path";
+            // 
+            // btnFeaturesFolder
+            // 
+            this.btnFeaturesFolder.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnFeaturesFolder.Location = new System.Drawing.Point(801, 17);
+            this.btnFeaturesFolder.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnFeaturesFolder.Name = "btnFeaturesFolder";
+            this.btnFeaturesFolder.Size = new System.Drawing.Size(36, 23);
+            this.btnFeaturesFolder.TabIndex = 3;
+            this.btnFeaturesFolder.Text = "...";
+            this.btnFeaturesFolder.UseVisualStyleBackColor = true;
+            this.btnFeaturesFolder.Click += new System.EventHandler(this.btnFeaturesFolder_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRun.Location = new System.Drawing.Point(620, 513);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(69, 29);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSortByName);
+            this.groupBox2.Controls.Add(this.btnDown);
+            this.groupBox2.Controls.Add(this.btnUp);
+            this.groupBox2.Controls.Add(this.lbxSort);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(26, 301);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(843, 208);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sort";
+            // 
+            // btnSortByName
+            // 
+            this.btnSortByName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortByName.Location = new System.Drawing.Point(43, 0);
+            this.btnSortByName.Name = "btnSortByName";
+            this.btnSortByName.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btnSortByName.Size = new System.Drawing.Size(58, 22);
+            this.btnSortByName.TabIndex = 13;
+            this.btnSortByName.Text = "Name↑";
+            this.btnSortByName.UseVisualStyleBackColor = true;
+            this.btnSortByName.Visible = false;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(6, 129);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btnDown.Size = new System.Drawing.Size(23, 34);
+            this.btnDown.TabIndex = 12;
+            this.btnDown.Text = "↓";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.Location = new System.Drawing.Point(6, 65);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btnUp.Size = new System.Drawing.Size(24, 34);
+            this.btnUp.TabIndex = 11;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // lbxSort
+            // 
+            this.lbxSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxSort.FormattingEnabled = true;
+            this.lbxSort.Location = new System.Drawing.Point(43, 31);
+            this.lbxSort.Name = "lbxSort";
+            this.lbxSort.Size = new System.Drawing.Size(795, 160);
+            this.lbxSort.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnFeaturesFolder);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Controls.Add(this.txtFeaturesPath);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(26, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Size = new System.Drawing.Size(843, 267);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Feature Browse";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtResults);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.Location = new System.Drawing.Point(32, 546);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox3.Size = new System.Drawing.Size(843, 174);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Results";
+            // 
+            // txtResults
+            // 
+            this.txtResults.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResults.Location = new System.Drawing.Point(43, 21);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(3, 3, 1, 3);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.Size = new System.Drawing.Size(795, 139);
+            this.txtResults.TabIndex = 2;
+            this.txtResults.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(74, 520);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Project Path";
+            // 
+            // btnProjectPath
+            // 
+            this.btnProjectPath.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnProjectPath.Location = new System.Drawing.Point(542, 515);
+            this.btnProjectPath.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnProjectPath.Name = "btnProjectPath";
+            this.btnProjectPath.Size = new System.Drawing.Size(31, 23);
+            this.btnProjectPath.TabIndex = 16;
+            this.btnProjectPath.Text = "...";
+            this.btnProjectPath.UseVisualStyleBackColor = true;
+            this.btnProjectPath.Click += new System.EventHandler(this.btnProjectPath_Click);
+            // 
+            // txtProjectPath
+            // 
+            this.txtProjectPath.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtProjectPath.Location = new System.Drawing.Point(178, 517);
+            this.txtProjectPath.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtProjectPath.Name = "txtProjectPath";
+            this.txtProjectPath.Size = new System.Drawing.Size(360, 21);
+            this.txtProjectPath.TabIndex = 14;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(909, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 739);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnProjectPath);
+            this.Controls.Add(this.txtProjectPath);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Test Runner";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox txtFeaturesPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnFeaturesFolder;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSortByName;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.ListBox lbxSort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox txtResults;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnProjectPath;
+        private System.Windows.Forms.TextBox txtProjectPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ TestRunner/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace TestRunner
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            folderBrowserDialog1.SelectedPath = txtFeaturesPath.Text;
+            folderBrowserDialog2.SelectedPath = txtProjectPath.Text;
+            BindTree();
+
+            //不检查线程
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnFeaturesFolder_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result.ToString() == "OK")
+            {
+                txtFeaturesPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void txtFolderPath_TextChanged(object sender, EventArgs e)
+        {
+            BindTree();
+        }
+
+        private void BindTree()
+        {
+            string dir = folderBrowserDialog1.SelectedPath;
+            if (string.IsNullOrWhiteSpace(dir))
+                return;
+
+            treeView1.Nodes.Clear();
+            var node = treeView1.Nodes.Add(dir);
+            AddFolder(dir, node);
+
+            if (treeView1.Nodes.Count > 0)
+                treeView1.Nodes[0].Expand();
+        }
+
+        private void AddFolder(string folderPath, TreeNode parentNode)
+        {
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            var fis = di.GetFiles();
+            foreach (var fi in fis)
+            {
+                if (fi.Extension.ToLower().Trim() == ".feature")
+                    parentNode.Nodes.Add(fi.FullName);
+            }
+            var fds = di.GetDirectories();
+            foreach (var fd in fds)
+            {
+                var node = parentNode.Nodes.Add(fd.FullName);
+
+                AddFolder(fd.FullName, node);
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            CheckChildren(e.Node);
+
+            if (e.Node.Text.Trim().ToLower().EndsWith(".feature"))
+            {
+                if (e.Node.Checked)
+                {
+                    if (!lbxSort.Items.Contains(e.Node.Text))
+                        lbxSort.Items.Insert(lbxSort.Items.Count, e.Node.Text);
+                }
+                else
+                {
+                    if (lbxSort.Items.Contains(e.Node.Text))
+                        lbxSort.Items.Remove(e.Node.Text);
+                }
+            }
+
+            treeView1.Enabled = true;
+        }
+
+
+        private void CheckChildren(TreeNode parentNode)
+        {
+            if (parentNode.Nodes.Count > 0)
+            {
+                foreach (TreeNode child in parentNode.Nodes)
+                {
+                    child.Checked = parentNode.Checked;
+                    CheckChildren(child);
+                }
+            }
+        }
+
+        private void treeView1_BeforeCheck(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView1.Enabled = false;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            int index = lbxSort.SelectedIndex;
+            if (index > 0)
+            {
+                for (int i = 0; i < lbxSort.SelectedItems.Count; i++)
+                {
+                    var tmp = lbxSort.SelectedItems[i];
+                    lbxSort.Items.Remove(lbxSort.SelectedItems[i]);
+                    lbxSort.Items.Insert(index - 1, tmp);
+                    lbxSort.SetSelected(index - 1, true);
+                }
+            }
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            int index = lbxSort.SelectedIndex;
+            if (index < lbxSort.Items.Count - 1)
+            {
+                for (int i = 0; i < lbxSort.SelectedItems.Count; i++)
+                {
+                    var tmp = lbxSort.SelectedItems[i];
+                    lbxSort.Items.Remove(lbxSort.SelectedItems[i]);
+                    lbxSort.Items.Insert(index + 1, tmp);
+                    lbxSort.SetSelected(index + 1, true);
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            DateTime st = DateTime.Now;
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(txtProjectPath.Text))
+                    return;
+                btnRun.Enabled = false;
+                txtResults.SelectionFont = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                txtResults.SelectionColor = Color.Black;
+                txtResults.AppendText(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss ") + "Running Start.");
+                txtResults.AppendText(Environment.NewLine);
+
+                //读取配置类
+                string configPath = Path.Combine(txtProjectPath.Text, @"src\test\java\CucumberTest\TestRunner.java");
+                string configText = File.ReadAllText(configPath);
+
+                foreach (var item in lbxSort.Items)
+                {
+                    //改写配置类
+                    string path = item.ToString();
+                    if (!path.ToLower().EndsWith(".feature"))
+                        continue;
+                    using (var feature = File.Open(path, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader rd = new StreamReader(feature))
+                        {
+                            for (; ; )
+                            {
+                                string s = rd.ReadLine().Trim();
+                                if (s.StartsWith("@"))
+                                {
+                                    string newConfig = Regex.Replace(configText, @"""@[^ ""]*""", '"' + s + '"');
+                                    File.WriteAllText(configPath, newConfig);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    //调用maven跑用例
+                    string batName = "mavenRun.bat";
+                    var fs = File.CreateText(batName);
+
+                    fs.WriteLine(txtProjectPath.Text.Trim().Substring(0, 2));
+                    fs.WriteLine(string.Format(@"cd ""{0}""", txtProjectPath.Text));
+                    fs.WriteLine("mvn test");
+                    fs.WriteLine("pause");
+                    fs.Close();
+                    var pcs = Process.Start(new ProcessStartInfo() { FileName = batName, Verb = "runas" });
+
+                    txtResults.SelectionFont = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                    txtResults.SelectionColor = Color.RoyalBlue;
+                    txtResults.AppendText(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss ") + "Running : " + item);
+                    txtResults.AppendText(Environment.NewLine);
+
+                    var so = DateTime.Now;
+
+                    pcs.WaitForExit();
+
+                    txtResults.SelectionFont = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                    txtResults.SelectionColor = Color.RoyalBlue;
+                    txtResults.AppendText(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss ") + "Done.");
+                    txtResults.AppendText("Cost time : " + (DateTime.Now - so).TotalSeconds + " sec");
+                    txtResults.AppendText(Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                txtResults.SelectionFont = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                txtResults.SelectionColor = Color.Red;
+                txtResults.AppendText(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss ") + " " + ex.Message);
+                txtResults.AppendText(Environment.NewLine);
+            }
+            finally
+            {
+
+                txtResults.SelectionFont = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+                txtResults.SelectionColor = Color.Black;
+                txtResults.AppendText(DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss ") + "Running finish.");
+                txtResults.AppendText("Cost time : " + (DateTime.Now - st).TotalMinutes + " min");
+                txtResults.AppendText(Environment.NewLine);
+                btnRun.Enabled = true;
+            }
+        }
+
+        private void btnProjectPath_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog2.ShowDialog();
+            if (result.ToString() == "OK")
+            {
+                txtProjectPath.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test Runner, batch testing tool, made by John Yue, 2018.");
+        }
+    }
+}
